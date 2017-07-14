9aa4802fa9074389898c04919eb35aad0a03e88b
Minghua Sun <clairesunstudio@gmail.com>
2017-07-14 16:48:00 -0400

    add key prop to VideListItem

--- src/component/videoList.js
+++ src/component/videoList.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import VideoListItem from './videoListItem'
 
 const VideoList = (props) => {
+  var videoItems = props.videos.map((video) => {
+    return <VideoListItem key={video.etag} video={video} />
+  })
   //props will arrive as argument to the functional component
   console.log(props)
   return(
-    <ul className="col-md-4 list-group">
-      {props.videos.length}
+    <ul className="list-inline">
+      {videoItems}
     </ul>
   )
 }
