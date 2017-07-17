53074a0a21e95c1921babc96dcb4184a221184ca
Minghua Sun <clairesunstudio@gmail.com>
2017-07-17 16:09:51 -0400

    use lodash debounce to throttle search every 300 milliseconds

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './component/SearchBar'
@@ -32,9 +33,11 @@ class App extends Component {
   }
 
   render() {
+    const videoSearch=_.debounce((term) => {this.videoSearch(term)}, 300)
+    //debounce takes the inner function and returns a new function runs every 300 milliseconds elaps
     return (
       <div>
-        <SearchBar onSearchTermChange={(term) => this.videoSearch(term)}/>
+        <SearchBar onSearchTermChange={videoSearch}/>
           <VideoDetail video={this.state.selectedVideo}/>
           <VideoList
             onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
